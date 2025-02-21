be803151a1a2a6855fe9bb05255ddd5c787740d4
Rafael Musmanno <rafa.musmanno@gmail.com>
2025-02-20 22:44:16 -0300

    feat: rotating point around origin

--- src/pdf/polygons/point.ts
+++ src/pdf/polygons/point.ts
@@ -6,4 +6,21 @@ export default class Point {
         this.x = x;
         this.y = y;
     }
+
+    rotate(angle:number, origin: Point) {
+        // rotation equation
+        // X = x*cos(θ) - y*sin(θ)
+        // Y = x*sin(θ) + y*cos(θ)
+        
+        // translate to origin
+        let point = new Point(this.x - origin.x, this.y - origin.y);
+
+        // rotate
+        let x = Math.floor(point.x * Math.cos(angle) - point.y * Math.sin(angle));
+        let y = Math.floor(point.x * Math.sin(angle) + point.y * Math.cos(angle));
+
+        // translate back from origin
+        this.x = x + origin.x;
+        this.y = y + origin.y;
+    }
 }
